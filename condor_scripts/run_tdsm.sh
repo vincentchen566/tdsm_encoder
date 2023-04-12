d9906ebe51dc57a9901258f2fce9c833f745c0a6
Joshuha <jthomasw@cern.ch>
2023-04-12 14:55:24 +0200

    working code generates padding, model runs on padded examples and masks zero-padded values. Plotting machinery also now masks out zero padded entries. Rescaling and unscaling now also works as desired.

--- condor_scripts/run_tdsm.sh
+++ condor_scripts/run_tdsm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /cvmfs/sft.cern.ch/lcg/views/LCG_102b_cuda/x86_64-centos7-gcc8-opt/setup.sh
 python3 -V
-python3 /afs/cern.ch/work/j/jthomasw/private/NTU/fast_sim/tdsm_encoder/trans_tdsm.py -o '/afs/cern.ch/work/j/jthomasw/private/NTU/fast_sim/tdsm_encoder' -s 0010
+python3 /afs/cern.ch/work/j/jthomasw/private/NTU/fast_sim/tdsm_encoder/trans_tdsm.py -o '/afs/cern.ch/work/j/jthomasw/private/NTU/fast_sim/tdsm_encoder' -s 0100
