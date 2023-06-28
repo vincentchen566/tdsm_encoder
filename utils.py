99b3a951950c80946978de8b6d9245fe7fff6287
Joshuha <jthomasw@cern.ch>
2023-06-28 17:23:50 +0200

    Tidy up utils code

--- utils.py
+++ utils.py
@@ -2,21 +2,15 @@ import h5py, math, torch, fnmatch, os
 import numpy as np
 from torch.nn.functional import normalize
 from torch.utils.data import Dataset
+from sklearn.preprocessing import RobustScaler
 
 class cloud_dataset(Dataset):
   def __init__(self, filename, transform=None, transform_y=None, device='cpu'):
     loaded_file = torch.load(filename, map_location=torch.device(device))
     self.data = loaded_file[0]
-    print(f'Loading {filename}: {type(loaded_file[0])}, {type(loaded_file[1])}')
-    
-    if 'toy_model' in filename:
-      self.condition = loaded_file[1].clone().detach()
-      self.min_y = torch.min(self.condition)
-      self.max_y = torch.max(self.condition)
-    else:
-      self.condition = torch.as_tensor(loaded_file[1]).float()
-      self.min_y = torch.min(self.condition)
-      self.max_y = torch.max(self.condition)
+    self.condition = torch.as_tensor(loaded_file[1]).float()
+    self.min_y = torch.min(self.condition)
+    self.max_y = torch.max(self.condition)
 
     self.transform = transform
     self.transform_y = transform_y
@@ -57,12 +51,26 @@ class rescale_energies:
             rescaled_e = torch.log(x/(1-x))
             rescaled_e = torch.nan_to_num(rescaled_e)
             rescaled_e = torch.reshape(rescaled_e,(-1,))
-            #print(f'features[:,1] {type(features[:,1])}, {features[:,1].shape}: {features[:,1]}')
+            
+            X_ = np.asarray(features[:,1]).reshape(-1, 1)
+            Y_ = np.asarray(features[:,2]).reshape(-1, 1)
+            
+            transform_x = RobustScaler().fit(X_)
+            transform_y = RobustScaler().fit(Y_)
+            
+            x_ = transform_x.transform(X_)
+            x_ = torch.from_numpy( x_.flatten() )
+            
+            y_ = transform_y.transform(Y_)
+            y_ = torch.from_numpy( y_.flatten() )
+
             x_ = normalize(features[:,1], dim=0)
             y_ = normalize(features[:,2], dim=0)
+
             z_ = features[:,3]
             # Stack tensors along the 'hits' dimension -1 
             stack_ = torch.stack((rescaled_e,x_,y_,z_), -1)
+            
             self.features = stack_
             
             return self.features
