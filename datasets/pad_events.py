307f9276ed9825f544baabb76809b286e352546b
Joshuha <jthomasw@cern.ch>
2023-08-02 16:39:18 +0200

    Updated pad_events.py so that single file is used to fit pre-processor transformation function. Produces smooth distributions for hit energy for all buckets and no disjoint part at low energy. Also now using PowerTransformer as the Gaussian shape is nice.

--- datasets/pad_events.py
+++ datasets/pad_events.py
@@ -25,13 +25,18 @@ def main():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print('Device: ', device)
     
-    E_ = np.array([])
-    X_ = np.array([])
-    Y_ = np.array([])
-    Z_ = np.array([])
+    # Fit the transformation functions before adding any padding to the dataset
+    E_ = []
+    X_ = []
+    Y_ = []
+    Z_ = []
     all_files_inenergy = torch.tensor([])
+    min_val_subset = 10
+    min_val = 10
     for infile in os.listdir(indir):
-        if fnmatch.fnmatch(infile, 'dataset_1_photons_tensor_no_pedding_euclidian_nentry*.pt'):
+        # It seems that using a single file to fit the transformation function,
+        # creates a stable result and smooth distributions for all files
+        if fnmatch.fnmatch(infile, 'dataset_1_photons_tensor_no_pedding_euclidian_nentry12*.pt'):
             filename = os.path.join(indir,infile)
             print(f'filename: {filename}')
             loaded_file = torch.load(filename)
@@ -39,29 +44,52 @@ def main():
             print(f'# showers: {len(loaded_file[0])}')
             nshowers = 0
             for shower in loaded_file[0]:
-                if nshowers%100 == 0:
-                    E_ = np.append(E_,np.asarray(shower[:,0] * GeV).reshape(-1, 1))
-                    X_ = np.append(X_,np.asarray(shower[:,1]).reshape(-1, 1))
-                    Y_ = np.append(Y_,np.asarray(shower[:,2]).reshape(-1, 1))
-                    Z_ = np.append(Z_,np.asarray(shower[:,3]).reshape(-1, 1))
-                nshowers+=1
+                
+                if sorted(set(shower[:,0]))[0] < min_val:
+                    min_val = sorted(set(shower[:,0]))[0]
+                if nshowers%2 == 0:
+                    if sorted(set(shower[:,0]))[0] < min_val_subset:
+                        min_val_subset = sorted(set(shower[:,0]))[0]
+                    E_.extend( shower[:,0]*GeV )
+                    X_.extend( shower[:,1] )
+                    Y_.extend( shower[:,2] )
+                    Z_.extend( shower[:,3] )
+                    nshowers+=1
             
             process = psutil.Process(os.getpid())
             print('Memory usage of current process 0 [GB]: ', process.memory_info().rss/(1024 * 1024 * 1024))
+    print(f'min_val_subset: {min_val_subset}')
+    print(f'min_val: {min_val}')
+    print(f'# showers: {len(E_)}')
+    E_ = np.array(E_).reshape(-1, 1)
+    X_ = np.array(X_).reshape(-1, 1)
+    Y_ = np.array(Y_).reshape(-1, 1)
+    Z_ = np.array(Z_).reshape(-1, 1)
     
-    E_ = E_.reshape(-1, 1)
-    X_ = X_.reshape(-1, 1)
-    Y_ = Y_.reshape(-1, 1)
-    Z_ = Z_.reshape(-1, 1)
-    
-    # Transform inputs
+    # Fit the transformation functions
     if transform == 1:
-        transform_e = QuantileTransformer(output_distribution='normal').fit(E_)
-        transform_x = QuantileTransformer(output_distribution='normal').fit(X_)
-        transform_y = QuantileTransformer(output_distribution='normal').fit(Y_)
+        print(f'Fitting transformation function for hit energies')
+        #transform_e = QuantileTransformer(output_distribution='normal').fit(E_)
+        #transform_e = QuantileTransformer(output_distribution='uniform').fit(E_)
+        #transform_e = RobustScaler().fit(E_)
+        transform_e = PowerTransformer(method="box-cox").fit(E_)
+        print(f'Fitting transformation function for hit X')
+        #transform_x = QuantileTransformer(output_distribution='normal').fit(X_)
+        #transform_x = QuantileTransformer(output_distribution='uniform').fit(X_)
+        #transform_x = RobustScaler().fit(X_)
+        transform_x = PowerTransformer(method="yeo-johnson").fit(X_)
+        print(f'Fitting transformation function for hit Y')
+        #transform_y = QuantileTransformer(output_distribution='normal').fit(Y_)
+        #transform_y = QuantileTransformer(output_distribution='uniform').fit(Y_)
+        #transform_y = RobustScaler().fit(Y_)
+        transform_y = PowerTransformer(method="yeo-johnson").fit(Y_)
     
     all_files_inenergy = torch.reshape(all_files_inenergy, (-1,1))
-    rescaler_y = QuantileTransformer(output_distribution='normal').fit(all_files_inenergy)
+    print(f'Fitting transformation function for incident energies')
+    #rescaler_y = QuantileTransformer(output_distribution='normal').fit(all_files_inenergy)
+    #rescaler_y = QuantileTransformer(output_distribution='uniform').fit(all_files_inenergy)
+    #rescaler_y = RobustScaler().fit(all_files_inenergy)
+    rescaler_y = PowerTransformer("box-cox").fit(all_files_inenergy)
     
     # For each file
     for infile in os.listdir(indir):
@@ -83,7 +111,7 @@ def main():
             print(f'{type(showers)} of {len(showers)} {type(showers[0])} showers')
             print(f'{type(incident_energies)} of {len(incident_energies)} {type(incident_energies[0])} incidient energies')
 
-            # Have to homogenise with data padding before using dataloaders
+            # Find the maximum number of hits for a shower in the dataset
             max_nhits = 0
             custom_data = utils.cloud_dataset(filename, device=device)
             padded_showers = []
@@ -91,10 +119,11 @@ def main():
                 if shower.shape[0] > max_nhits:
                     max_nhits = shower.shape[0]
             
-            incident_energies = torch.reshape(incident_energies, (-1,1))
-            if transform == 1:
+            # Transform the incident energies
+            incident_energies = np.asarray( incident_energies ).reshape(-1, 1)
+            #if transform == 1:
                 # Rescale the conditional input for each shower
-                incident_energies = rescaler_y.transform(incident_energies)
+            #    incident_energies = rescaler_y.transform(incident_energies)
             incident_energies = torch.from_numpy( incident_energies.flatten() )
             
             # Rescale hit energy and position and do padding
@@ -109,6 +138,7 @@ def main():
                     print(f'incident e: {incident_energies[shower_count]} with {showers.shape[0]} hits')
                     continue
                 
+                # Transform the inputs
                 E_ = np.asarray(showers[:,0]*GeV).reshape(-1, 1)
                 X_ = np.asarray(showers[:,1]).reshape(-1, 1)
                 Y_ = np.asarray(showers[:,2]).reshape(-1, 1)
@@ -123,7 +153,7 @@ def main():
                 Z_ = showers[:,3]
                 shower_data_transformed = torch.stack((E_,X_,Y_,Z_), -1)
                 
-                # Pad with sentinel value to make the showers uniform in length
+                # Homogenise data with padding to make all showers the same length
                 padded_shower = F.pad(input = shower_data_transformed, pad=(0,0,0,pad_hits), mode='constant', value=-20)
                 padded_showers.append(padded_shower)
                 shower_count+=1
@@ -144,7 +174,12 @@ def main():
             print(f'{type(padded_showers)} of {len(padded_showers)} showers of type {type(padded_showers[0])}')
             print(f'{type(padded_incident_e)} of {len(padded_incident_e)} showers of type {type(padded_incident_e[0])}')
 
+            
+            
             #### Input plots ####
+            # Do you want to invert transformation for plotting purposes?
+            invert_transform = 0
+            
             print('Padded filename: ', outfilename)
             padded_data = utils.cloud_dataset(outfilename, device=device)
             plot_file = ofile.replace(".pt", "" )
@@ -154,7 +189,7 @@ def main():
                 os.makedirs(plots_path)
 
             # Load input data
-            padded_data_loader = DataLoader(padded_data,batch_size=1,shuffle=False)
+            padded_data_loader = DataLoader(padded_data, batch_size=1, shuffle=False)
             n_hits = []
             all_x = []
             all_y = []
@@ -178,12 +213,13 @@ def main():
                 real_hit_ys = torch.masked_select(hit_ys,mask)
                 real_hit_zs = torch.masked_select(hit_zs,mask)
                 
-                # Invert transformation for intuitive plots (also checks inversion works)
-                if transform == 1:
+                # Invert transformation for plots in original variable space (also checks inversion works)
+                if invert_transform == 1:
                     real_hit_energies = real_hit_energies.tolist()
                     real_hit_energies = np.asarray(real_hit_energies).reshape(-1, 1)
                     real_hit_energies = transform_e.inverse_transform(real_hit_energies)
                     real_hit_energies = torch.from_numpy( real_hit_energies.flatten() )
+                    
                     
                     real_hit_xs = real_hit_xs.tolist()
                     real_hit_xs = np.asarray(real_hit_xs).reshape(-1, 1)
@@ -195,30 +231,31 @@ def main():
                     real_hit_ys = transform_y.inverse_transform(real_hit_ys)
                     real_hit_ys = torch.from_numpy( real_hit_ys.flatten() )
                 
+                real_hit_energies = real_hit_energies.tolist()
+                real_hit_xs = real_hit_xs.tolist()
+                real_hit_ys = real_hit_ys.tolist()
                 real_hit_zs = real_hit_zs.tolist()
                 
-                n_hits.append( len(real_hit_energies) )
-                all_e.append(real_hit_energies)
-                all_x.append(real_hit_xs)
-                all_y.append(real_hit_ys)
-                all_z.append(real_hit_zs)
+                n_hits.extend( [len(real_hit_energies)] )
+                all_e.extend(real_hit_energies)
+                all_x.extend(real_hit_xs)
+                all_y.extend(real_hit_ys)
+                all_z.extend(real_hit_zs)
                 hits_sum_e = sum(real_hit_energies)
-                total_shower_e.append( hits_sum_e.item() )
+                total_shower_e.extend( [hits_sum_e] )
                 
-                incident_energies = torch.reshape(incident_energies, (-1,1))
-                if transform == 1:
+                # Transform the incident energies
+                incident_energies = np.asarray( incident_energies ).reshape(-1, 1)
+                if invert_transform == 1:
                     # Rescale the conditional input for each shower
                     incident_energies = rescaler_y.inverse_transform(incident_energies)
                 incident_energies = torch.from_numpy( incident_energies.flatten() )
-                all_incident_e.append( incident_energies.item() )
+                all_incident_e.extend( [incident_energies.item()] )
             
-            plot_e = np.concatenate(all_e)
-            plot_x = np.concatenate(all_x)
-            plot_y = np.concatenate(all_y)
-            plot_z = np.concatenate(all_z)
-            
-            #all_incident_e = np.concatenate(all_incident_e)
-            #total_shower_e = np.concatenate(total_shower_e)
+            plot_e = all_e
+            plot_x = all_x
+            plot_y = all_y
+            plot_z = all_z
 
             fig, ax = plt.subplots(3,3, figsize=(14,14))
             ax[0][0].set_ylabel('# entries')
@@ -261,8 +298,11 @@ def main():
             ax[2][0].hist(total_shower_e, 30, label='Geant4')
             ax[2][0].set_yscale('log')
             ax[2][0].legend(loc='upper right')
-
-            fig.savefig(plots_path+'hit_inputs.png')
+            if invert_transform == 0:
+                fig_save_name = plots_path+'hit_inputs_transformed.png'
+            else:
+                fig_save_name = plots_path+'hit_inputs_non_transformed.png'
+            fig.savefig(fig_save_name)
 
 if __name__=='__main__':
     main()
