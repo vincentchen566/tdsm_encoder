576c18883f17ecc12024a712847a72f36bcb918d
Joshuha <jthomasw@cern.ch>
2024-04-28 18:27:36 +0200

    Large code restructuring to run WandB sweeps. Mostly moving training/sampling code into their own functions. configs directory contains example sweep yaml files.

--- condor_scripts/run_tdsm.sh
+++ condor_scripts/run_tdsm.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-source /cvmfs/sft.cern.ch/lcg/views/LCG_102b_cuda/x86_64-centos7-gcc8-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_104a_cuda/x86_64-el9-gcc11-opt/setup.sh
 python3 -V
-python3 /afs/cern.ch/work/j/jthomasw/private/NTU/fast_sim/tdsm_encoder/trans_tdsm.py -o '/afs/cern.ch/work/j/jthomasw/private/NTU/fast_sim/tdsm_encoder' -s 0010 -i power_transformer
+echo "$(ls)"
+echo "$(nvidia-smi -L)"
+echo $CUDA_VISIBLE_DEVICES
+CFG_FILE=$(echo ${1} | cut -d '/' -f 3)
+echo $CFG_FILE
+python3 trans_tdsm.py -s 0010 -i ds2_diff_transforms -c $CFG_FILE
