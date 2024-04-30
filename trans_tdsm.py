e7eded943f4ae23a88f1b2de66c4293da4e01825
AlbertHsuNTUphys <b07202037@ntu.edu.tw>
2024-04-30 16:00:14 +0200

    [wandb] add calo plot

--- trans_tdsm.py
+++ trans_tdsm.py
@@ -661,6 +661,8 @@ def main(config=None):
    
             print(('python util/evaluate_image_based.py --output_dir {outdir} --input_file {Gen_file} --reference_file {Geant4_file} --dataset 2'.format(Gen_file = os.path.join(output_directory, 'Gen.h5'), Geant4_file = os.path.join(output_directory, 'Reference.h5'), outdir = os.path.join(output_directory, 'calo_score'))))
             os.system('python util/evaluate_image_based.py --output_dir {outdir} --input_file {Gen_file} --reference_file {Geant4_file} --dataset 2'.format(Gen_file = os.path.join(output_directory, 'Gen.h5'), Geant4_file = os.path.join(output_directory, 'Reference.h5'), outdir = os.path.join(output_directory, 'calo_score')))
+            wandb.log({"summary" : wandb.Image(os.path.join(output_directory, 'calo_score', 'reference_average_shower_dataset_2.png'))})
+            wandb.log({"summary" : wandb.Image(os.path.join(output_directory, 'calo_score', 'average_shower_dataset_2.png'))})
 
 
 if __name__=='__main__':
